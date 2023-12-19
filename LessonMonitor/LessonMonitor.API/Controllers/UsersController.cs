1222f4f508c98e70622932497640598aa0f7108e
Serhii Artemenko <sergartem.sa@gmail.com>
2023-12-19 11:02:25 +0200

    Add base app

---
+++ LessonMonitor/LessonMonitor.API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LessonMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController   : ControllerBase
+    {
+        [HttpGet]
+        public User[] Get(string userName)
+        {
+            var rnd = new Random();
+            var users = new List<User>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var user = new User
+                {
+                    Name = userName + i,
+                    Age = rnd.Next(18, 60)
+                };
+
+                users.Add(user);
+            }
+
+            return users.ToArray();
+        }
+    }
+}
